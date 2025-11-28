14ca123836bbd3eb8199ee5e7374c2bc2966b16c
Charlotte-encn <charlotte.encarnacao@etu.univ.fr>
2025-11-28 15:41:29 +0100

    Création des dossiers pricipaux

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <body>
     <header>
         <div class="logo">
-            <img src="images/logo.png" alt="Logo TokaFest">
+            <img src="images/logo.jpg" alt="Logo TokaFest">
         </div>
         <nav>
             <ul>        
